f87259717908903e547b1e24906244e6ca05f866
Dmitrii Mamonov <dm.ggl2022@gmail.com>
2025-04-19 22:16:13 +0400

    add listing editor

--- app/routes/update-listing.tsx
+++ app/routes/update-listing.tsx
@@ -1,10 +1,122 @@
-export default function UpdateListing() {
+import { z } from 'zod';
+import { getZodConstraint, parseWithZod } from '@conform-to/zod';
+import { getDB } from '~/utils/drizzle';
+import type { Route } from './+types/update-listing';
+import { listings, listingToCategory } from 'drizzle/schema';
+import { eq } from 'drizzle-orm';
+import { Form, redirect } from 'react-router';
+import { appRoute } from '~/routes';
+import {
+  getFormProps,
+  getInputProps,
+  getSelectProps,
+  getTextareaProps,
+  useForm,
+} from '@conform-to/react';
+import { FormErrorList } from '~/components/form-error-list';
+
+const titleMaxLength = 100;
+const descriptionMaxLength = 500;
+
+export async function loader({ context, params }: Route.LoaderArgs) {
+  const id = params.listingId;
+  if (id === undefined) {
+    throw new Response('No listingId param', { status: 400 });
+  }
+
+  const db = getDB(context.cloudflare.env.DB);
+
+  const listing = await db.query.listings.findFirst({
+    where: (lisings, { eq }) => eq(lisings.id, +id),
+    with: {
+      categories: {
+        columns: {
+          categoryId: true,
+        },
+      },
+    },
+  });
+
+  const categories = await db.query.categories.findMany();
+
+  if (!listing) {
+    throw new Response('Listing not found', { status: 404 });
+  }
+
+  return { listing, categories };
+}
+
+const listingEditorSchema = z.object({
+  title: z.string().min(1).max(titleMaxLength),
+  description: z.string().min(1).max(descriptionMaxLength),
+  sum: z.number().min(0),
+  categoryId: z.number().min(1),
+});
+
+export async function action({ request, context, params }: Route.ActionArgs) {
+  const listingId = params.listingId;
+  if (listingId === undefined) {
+    throw new Response('No listingId param', { status: 400 });
+  }
+
+  const formData = await request.formData();
+
+  const submission = parseWithZod(formData, {
+    schema: listingEditorSchema,
+  });
+
+  if (submission.status !== 'success') {
+    return submission.reply();
+  }
+
+  const { title, description, sum, categoryId } = submission.value;
+
+  const db = getDB(context.cloudflare.env.DB);
+  await db
+    .update(listings)
+    .set({
+      title: title as string,
+      description: description as string,
+      sum: +sum as number,
+      updatedAt: new Date().toISOString(),
+    })
+    .where(eq(listings.id, +listingId));
+
+  if (categoryId) {
+    await db.delete(listingToCategory).where(eq(listingToCategory.listingId, +listingId));
+    await db.insert(listingToCategory).values({
+      listingId: +listingId,
+      categoryId,
+    });
+  }
+
+  return redirect(`${appRoute.myListings}`);
+}
+
+export default function UpdateListing({ loaderData, actionData }: Route.ComponentProps) {
+  const { listing, categories } = loaderData;
+  const lastResult = actionData;
+  const childCategories = categories.filter((c) => c.parentId !== null);
+
+  const [form, fields] = useForm({
+    lastResult,
+    constraint: getZodConstraint(listingEditorSchema),
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: listingEditorSchema });
+    },
+  });
+
+  const formControlBaseClassname = 'border-gray-200 p-2 aria-[invalid]:border-red-600';
+  const inputClassname = `rounded border ${formControlBaseClassname}`;
+
   return (
     <>
       <h1 className="title mb-6 text-center">Edit Item</h1>
       <div className="m-auto w-[600px] max-w-full rounded-2xl border border-gray-200 p-14 shadow-lg">
-        <form className="" method="POST">
-          <div className="mb-6">
+        <Form method="POST" {...getFormProps(form)}>
+          {/* <div className="mb-6">
             <input type="file" id="avatar" name="avatar" className="sr-only" />
             <label
               htmlFor="avatar"
@@ -12,63 +124,83 @@ export default function UpdateListing() {
             >
               <span>Upload Image</span>
             </label>
-          </div>
+          </div> */}
 
-          <div className="mb-10 flex flex-col gap-10">
+          <div className="mb-10 flex flex-col gap-4">
             <div className="flex flex-col gap-1">
-              <label htmlFor="ticket-name">Title</label>
+              <label htmlFor={fields.title.id}>Title</label>
               <input
-                type="text"
-                name="ticket-name"
-                id="ticket-name"
-                className="rounded border border-gray-200 p-2"
-                value="Lorem ipsum dolor"
+                className={inputClassname}
+                defaultValue={listing.title}
+                maxLength={titleMaxLength}
                 required
+                {...getInputProps(fields.title, { type: 'text' })}
               />
+              <div className="min-h-6">
+                <FormErrorList id={fields.title.errorId} errors={fields.title.errors} />
+              </div>
             </div>
 
             <div className="flex flex-col gap-1">
-              <label htmlFor="comment-field">Description</label>
+              <label htmlFor={fields.description.id}>Description</label>
               <textarea
-                name="comment"
-                id="comment-field"
+                className={inputClassname}
                 cols={30}
                 rows={8}
-                maxLength={400}
-                minLength={50}
-                className="rounded border border-gray-200 p-2"
-                value="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sint optio sequi corporis ipsa, tempora ducimus consectetur fugiat necessitatibus ullam, voluptatum incidunt itaque expedita, assumenda ipsum deserunt perspiciatis blanditiis labore!"
+                defaultValue={listing.description}
+                maxLength={descriptionMaxLength}
+                required
+                {...getTextareaProps(fields.description)}
               />
+              <div className="min-h-6">
+                <FormErrorList
+                  id={fields.description.errorId}
+                  errors={fields.description.errors}
+                />
+              </div>
             </div>
 
             <div className="flex flex-col gap-1">
-              <label htmlFor="price-field">Price</label>
+              <label htmlFor={fields.sum.id}>Price</label>
               <input
-                type="number"
-                name="price"
-                id="price-field"
-                className="rounded border border-gray-200 p-2"
-                min={1}
-                value={1000}
+                className={inputClassname}
+                defaultValue={listing.sum}
                 required
+                {...getInputProps(fields.sum, { type: 'number' })}
               />
+              <div className="min-h-6">
+                <FormErrorList id={fields.sum.errorId} errors={fields.sum.errors} />
+              </div>
             </div>
 
-            <select
-              name="category"
-              id="category-field"
-              className="border-b border-gray-200 py-2"
-              value={1}
-            >
-              <option value={''}>Select category</option>
-              <option value={1}>Books</option>
-              <option value={2}>Clothing</option>
-            </select>
+            <div className="flex flex-col gap-1">
+              <label className="" htmlFor={fields.categoryId.id}>
+                Category
+              </label>
+              <select
+                className={`${formControlBaseClassname} border-b`}
+                defaultValue={listing.categories[0].categoryId}
+                required
+                {...getSelectProps(fields.categoryId)}
+              >
+                {childCategories.map(({ id, name }) => (
+                  <option key={id} value={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+              <div className="min-h-6">
+                <FormErrorList
+                  id={fields.categoryId.errorId}
+                  errors={fields.categoryId.errors}
+                />
+              </div>
+            </div>
           </div>
           <button className="button-base" type="submit">
             Update
           </button>
-        </form>
+        </Form>
       </div>
     </>
   );
